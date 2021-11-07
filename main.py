846367f197cc5f1ce5529f31950f470b840bff0c
Ahmed Abouslima <54246638+abouslima@users.noreply.github.com>
2021-11-07 14:02:29 +0100

    add app.yaml file

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ app = Flask(__name__)
 
 model = LHMModel()
 
-
 @app.route('/')
 def show_center():
     center = model.show_center()
@@ -14,17 +13,18 @@ def show_center():
 @app.route('/address', methods=["GET", "POST"])
 def get_address():
     global model
-    model = LHMModel()
     data = request.get_json()
-    if ("address" not in data) or (data["address"] == None):
+
+    if ("address" not in data.keys()) or (data["address"] is None):
         input_address = "Freddie-Mercury-Str. 5"
     else:
         input_address =  data["address"]
 
-    if ("distance" not in data) or (data["distance"] == None):
+    if ("distance" not in data.keys()) or (data["distance"] is None):
         input_distance = 1000
     else:
         input_distance =  data["distance"]
+
     model.geo_from_address(input_address)
     toilets = model.nearby_toilets(input_distance)
     print(input_address, input_distance)
@@ -32,12 +32,10 @@ def get_address():
     res.headers.add('Access-Control-Allow-Origin', '*')
     return res
 
-
-
 # @app.route('/mapped')
 # def mapped_toilets():
 #     mapped = model.map_nearby_toilets()
 #     return mapped
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='127.0.0.1', port=8080, debug=True)
