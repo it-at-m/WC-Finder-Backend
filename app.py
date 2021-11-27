294f77b7bfea50081a23150a1c7d1370c465896f
Ahmed Abouslima <54246638+abouslima@users.noreply.github.com>
2021-11-27 10:58:29 +0100

    Clean code

--- app.py
+++ app.py
@@ -3,7 +3,6 @@
 # gcloud run deploy --image gcr.io/lhm-14-dps/toilets --platform managed
 
 from flask import Flask, request, Response, send_file
-from flask_restful import Resource, Api
 from inference import LHMModel
 from flask_cors import CORS
 
@@ -43,31 +42,6 @@ def filtering():
     model.filter_key(data["EuroKey"])
     filtered_toilets = model.nearby_df.to_json(orient="records")
     return filtered_toilets
-
-
-# @app.route('/address', methods=["GET", "POST"])
-# def get_address():
-#     global model
-#     data = request.get_json()
-#
-#     if ("address" not in data.keys()) or (data["address"] is None):
-#         input_address = "Freddie-Mercury-Str. 5"
-#     else:
-#         input_address = data["address"]
-#
-#     if ("distance" not in data.keys()) or (data["distance"] is None):
-#         input_distance = 1000
-#     else:
-#         input_distance = data["distance"]
-#
-#     model.geo_from_address(input_address)
-#     toilets = model.nearby_toilets(input_distance)
-#     return toilets
-
-# @app.route('/mapped')
-# def mapped_toilets():
-#     mapped = model.map_nearby_toilets()
-#     return mapped
 
 
 if __name__ == '__main__':
--- inference.py
+++ inference.py
@@ -1,15 +1,10 @@
-import folium
 import pandas as pd
-from geopy.geocoders import GoogleV3
-from pyproj import Geod
-from shapely.geometry import Point, LineString
-from flask_restful import Resource, Api
 
-class LHMModel(Resource):
+
+class LHMModel:
 
     def __init__(self):
-        self.df = pd.read_json('./preprocessing/toilets_v7.json')
-        # self.current = None
+        self.df = pd.read_json('./preprocessing/toilets_v8.json')
         self.nearby_df = None
 
     def show_all(self):
@@ -27,29 +22,3 @@ class LHMModel(Resource):
             pass
         else:
             self.nearby_df = self.nearby_df[self.nearby_df["eurokey"] == value]
-
-    # def geo_from_address(self, street_hn, city="Munich", country="Germany"):
-    #     locator = GoogleV3(api_key="AIzaSyBAu96ruSZwSGt8t8muUYlvGLmiHmSSeJQ")
-    #     location = locator.geocode(street_hn + ", " + city + ", " + country)
-    #     self.current = (location.latitude, location.longitude)
-    #
-    # def nearby_toilets(self, distance):
-    #     geod = Geod(ellps="WGS84")
-    #     self.nearby_df = self.df[self.df['geometry'].map(lambda x: geod.geometry_length(LineString([Point(x), self.current]))) < distance]
-    #     self.nearby_df = self.nearby_df.rename(columns={"geometry":"position"})
-    #     self.nearby_df = self.nearby_df[['title', 'short_description', 'position']]
-    #     result = self.nearby_df.to_json(orient="records")
-    #     return result
-
-    # def map_nearby_toilets(self):
-    #     my_map = folium.Map(location=self.current, zoom_start=15)
-    #     folium.Marker(location=self.current, icon=folium.Icon(
-    #                               icon_color='green', #acceptable colors are thos in color_options set.
-    #                               prefix='glyphicon')).add_to(my_map)
-    #     for index, location_info in self.nearby_df.iterrows():
-    #         folium.Marker(location=(location_info['latitude'], location_info["longitude"]), popup=location_info["title"]).add_to(my_map)
-    #
-    #     return my_map._repr_html_()
-
-    # def filter_further(self):
-    #     pass
